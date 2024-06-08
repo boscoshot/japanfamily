60ae9b1b2d1a1f3a61865a79e8a61a3d97c66b39
boscoshot <113295603+boscoshot@users.noreply.github.com>
2024-06-08 19:25:48 +0200

    Add files via upload

---
+++ culture-japonaise.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Le Japon en famille</title>
+    <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+ 
+  </head>
+  <body>
+  <?php include 'header.php'; ?>
+    <nav id="navbar-example2" class="navbar bg-body-tertiary px-3 mb-3 sticky-top">
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a class="nav-link" href="#scrollspyHeading1">La culture japonaise en général</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#scrollspyHeading2">Les choses à savoir dans les transports</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#scrollspyHeading3">Les choses à savoir dans la rue</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#scrollspyHeading4">Les choses à savoir quand vous échangez avec un japonais</a>
+        </li>
+      </ul>
+    </nav>
+    <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="scrollspy-example bg-body-tertiary p-3 rounded-2" tabindex="0">
+      <h4 id="scrollspyHeading1">La culture japonaise en général</h4>
+      <img src="https://japon-fr.com/images/large/culture-japonaise.jpg" class="img-fluid">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae placerat velit, in pretium neque. Curabitur nec tempus sem, quis interdum ipsum. Cras ac ultricies ipsum. Ut et nisl vel risus feugiat condimentum quis vel nisi. Aliquam erat volutpat. Nunc lacus nulla, mollis non leo non, ullamcorper dictum nisi. Morbi vulputate velit accumsan quam pulvinar sagittis. Nullam malesuada nunc quis neque finibus, a venenatis quam feugiat. Praesent lorem leo, pulvinar quis ultricies at, elementum sit amet lorem. Vestibulum bibendum neque cursus augue tempus, lobortis molestie dui cursus. Vestibulum mi erat, laoreet et consequat nec, sodales at elit. Fusce sagittis, ligula eu accumsan vehicula, enim massa gravida orci, quis aliquam arcu ante ac ex. Quisque pulvinar sem non vulputate bibendum. Pellentesque enim libero, molestie ac varius et, cursus non mi. Vestibulum justo sem, dictum quis commodo ut, finibus sit amet metus. Integer eu enim quis sem lobortis gravida.
+
+        Sed quis luctus urna, vitae finibus lectus. In non mattis quam. Vestibulum urna erat, aliquam sit amet suscipit in, vestibulum ac nunc. In ullamcorper vehicula justo, sed ornare nulla imperdiet nec. Nullam nunc quam, condimentum non efficitur vitae, mollis ut sapien. Mauris ut est posuere, congue nisl in, dignissim leo. Aenean iaculis leo vitae lacus pulvinar dapibus. Cras a felis ac augue bibendum commodo non sit amet urna. Nullam felis sapien, laoreet eget finibus mattis, pellentesque non quam. Maecenas libero metus, lacinia id sem vel, fringilla fermentum erat. Maecenas tortor lectus, suscipit vel erat sed, lacinia sodales urna. Integer sapien diam, fermentum id laoreet ut, egestas vel magna.</p>
+      <h4 id="scrollspyHeading2">Les choses à savoir dans les transports</h4>
+      <img src="https://images.rtl.fr/~c/2000v2000/rtl/www/1415887-un-shinkansen-train-japonais-a-tokyo-le-21-03-2021.jpg" class="img-fluid">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae placerat velit, in pretium neque. Curabitur nec tempus sem, quis interdum ipsum. Cras ac ultricies ipsum. Ut et nisl vel risus feugiat condimentum quis vel nisi. Aliquam erat volutpat. Nunc lacus nulla, mollis non leo non, ullamcorper dictum nisi. Morbi vulputate velit accumsan quam pulvinar sagittis. Nullam malesuada nunc quis neque finibus, a venenatis quam feugiat. Praesent lorem leo, pulvinar quis ultricies at, elementum sit amet lorem. Vestibulum bibendum neque cursus augue tempus, lobortis molestie dui cursus. Vestibulum mi erat, laoreet et consequat nec, sodales at elit. Fusce sagittis, ligula eu accumsan vehicula, enim massa gravida orci, quis aliquam arcu ante ac ex. Quisque pulvinar sem non vulputate bibendum. Pellentesque enim libero, molestie ac varius et, cursus non mi. Vestibulum justo sem, dictum quis commodo ut, finibus sit amet metus. Integer eu enim quis sem lobortis gravida.
+
+        Sed quis luctus urna, vitae finibus lectus. In non mattis quam. Vestibulum urna erat, aliquam sit amet suscipit in, vestibulum ac nunc. In ullamcorper vehicula justo, sed ornare nulla imperdiet nec. Nullam nunc quam, condimentum non efficitur vitae, mollis ut sapien. Mauris ut est posuere, congue nisl in, dignissim leo. Aenean iaculis leo vitae lacus pulvinar dapibus. Cras a felis ac augue bibendum commodo non sit amet urna. Nullam felis sapien, laoreet eget finibus mattis, pellentesque non quam. Maecenas libero metus, lacinia id sem vel, fringilla fermentum erat. Maecenas tortor lectus, suscipit vel erat sed, lacinia sodales urna. Integer sapien diam, fermentum id laoreet ut, egestas vel magna.</p>
+      <h4 id="scrollspyHeading3">Les choses à savoir dans la rue</h4>
+      <img src="https://previews.123rf.com/images/patrykkosmider/patrykkosmider1709/patrykkosmider170900088/85594484-tokyo-japon-12-novembre-2016-les-rues-occup%C3%A9es-du-quartier-de-shibuya-%C3%A0-tokyo-la-nuit-au-japon.jpg" class="img-fluid">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae placerat velit, in pretium neque. Curabitur nec tempus sem, quis interdum ipsum. Cras ac ultricies ipsum. Ut et nisl vel risus feugiat condimentum quis vel nisi. Aliquam erat volutpat. Nunc lacus nulla, mollis non leo non, ullamcorper dictum nisi. Morbi vulputate velit accumsan quam pulvinar sagittis. Nullam malesuada nunc quis neque finibus, a venenatis quam feugiat. Praesent lorem leo, pulvinar quis ultricies at, elementum sit amet lorem. Vestibulum bibendum neque cursus augue tempus, lobortis molestie dui cursus. Vestibulum mi erat, laoreet et consequat nec, sodales at elit. Fusce sagittis, ligula eu accumsan vehicula, enim massa gravida orci, quis aliquam arcu ante ac ex. Quisque pulvinar sem non vulputate bibendum. Pellentesque enim libero, molestie ac varius et, cursus non mi. Vestibulum justo sem, dictum quis commodo ut, finibus sit amet metus. Integer eu enim quis sem lobortis gravida.
+
+        Sed quis luctus urna, vitae finibus lectus. In non mattis quam. Vestibulum urna erat, aliquam sit amet suscipit in, vestibulum ac nunc. In ullamcorper vehicula justo, sed ornare nulla imperdiet nec. Nullam nunc quam, condimentum non efficitur vitae, mollis ut sapien. Mauris ut est posuere, congue nisl in, dignissim leo. Aenean iaculis leo vitae lacus pulvinar dapibus. Cras a felis ac augue bibendum commodo non sit amet urna. Nullam felis sapien, laoreet eget finibus mattis, pellentesque non quam. Maecenas libero metus, lacinia id sem vel, fringilla fermentum erat. Maecenas tortor lectus, suscipit vel erat sed, lacinia sodales urna. Integer sapien diam, fermentum id laoreet ut, egestas vel magna.</p>
+      <h4 id="scrollspyHeading4">Les choses à savoir quand vous échangez avec un japonais</h4>
+      <img src="https://d37sy4vufic209.cloudfront.net/website/_next/static/dynamic/47bf8c866b57f1464a9b1cda8878ea54/700.jpg" class="img-fluid">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae placerat velit, in pretium neque. Curabitur nec tempus sem, quis interdum ipsum. Cras ac ultricies ipsum. Ut et nisl vel risus feugiat condimentum quis vel nisi. Aliquam erat volutpat. Nunc lacus nulla, mollis non leo non, ullamcorper dictum nisi. Morbi vulputate velit accumsan quam pulvinar sagittis. Nullam malesuada nunc quis neque finibus, a venenatis quam feugiat. Praesent lorem leo, pulvinar quis ultricies at, elementum sit amet lorem. Vestibulum bibendum neque cursus augue tempus, lobortis molestie dui cursus. Vestibulum mi erat, laoreet et consequat nec, sodales at elit. Fusce sagittis, ligula eu accumsan vehicula, enim massa gravida orci, quis aliquam arcu ante ac ex. Quisque pulvinar sem non vulputate bibendum. Pellentesque enim libero, molestie ac varius et, cursus non mi. Vestibulum justo sem, dictum quis commodo ut, finibus sit amet metus. Integer eu enim quis sem lobortis gravida.
+
+        Sed quis luctus urna, vitae finibus lectus. In non mattis quam. Vestibulum urna erat, aliquam sit amet suscipit in, vestibulum ac nunc. In ullamcorper vehicula justo, sed ornare nulla imperdiet nec. Nullam nunc quam, condimentum non efficitur vitae, mollis ut sapien. Mauris ut est posuere, congue nisl in, dignissim leo. Aenean iaculis leo vitae lacus pulvinar dapibus. Cras a felis ac augue bibendum commodo non sit amet urna. Nullam felis sapien, laoreet eget finibus mattis, pellentesque non quam. Maecenas libero metus, lacinia id sem vel, fringilla fermentum erat. Maecenas tortor lectus, suscipit vel erat sed, lacinia sodales urna. Integer sapien diam, fermentum id laoreet ut, egestas vel magna.</p>
+
+    </div>
+    
+  <br><br>
+  <?php include 'footer.php'; ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
