07a37f4cdae3b5bbdd79c89db5fbfc8fe4f1acc1
boscoshot <113295603+boscoshot@users.noreply.github.com>
2024-06-09 14:32:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Le Japon en famille</title>
     <?php include 'link.php'; ?>
-    <meta name="description" content="Découvrez ce qu'il faut visiter en famille au Japon avec des enfants" />
-    <meta name="keywords" content="japon enfants famille" />
-    <meta name="robots" content="all" />
-    <meta name="language" content="fr" />
+    <meta name="description" content="Découvrez ce qu'il faut visiter en famille au Japon avec des enfants">
+    <meta name="keywords" content="japon enfants famille">
+    <meta name="robots" content="all">
+    <meta name="language" content="fr">
   </head>
   <body>
     <?php include 'header.php'; ?>
