bc508b9ea107938c12b8a6ccbcd7f83285265708
boscoshot <113295603+boscoshot@users.noreply.github.com>
2024-06-09 10:30:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -64,10 +64,7 @@
       </div>
     </div>
   </div>
-<div class="scroll-to-top-container">
-    <button id="scrollToTopBtn"><i class="fas fa-chevron-up"></i></button>
-</div>
-
+<button id="scrollToTopBtn" class="alignleft"><i class="fas fa-chevron-up"></i></button>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 <script>
