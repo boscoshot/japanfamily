41691e047ac964e01b1cf7c2c6c238f008749527
boscoshot <113295603+boscoshot@users.noreply.github.com>
2024-06-09 10:56:54 +0200

    Update preparation-voyage.php

--- preparation-voyage.php
+++ preparation-voyage.php
@@ -66,7 +66,7 @@ Enfin, assurez-vous que chaque enfant porte un bracelet d'identification ou qu'i
 
     </div>    
   <br><br>
-  <p class="aligncenter">Copyright Cyril Duport®</p>
+  <?php include 'footer.php'; ?>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
